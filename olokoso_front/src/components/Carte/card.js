4dbf18c4805725d87630cdc035f0a604fec36abe
annebequet <a.bequet@live.fr>
2021-03-02 17:38:21 +0100

    début stylisation menu

--- olokoso_front/src/components/Carte/card.js
+++ olokoso_front/src/components/Carte/card.js
@@ -24,9 +24,9 @@ const Card = ({
   id,
   description,
   ingredients,
-  produits,
   categories,
-  image_url,
+  disponibilite,
+  image,
 }) => {
   const [show, setShow] = useState(false);
   const dispath = useDispatch();
@@ -59,7 +59,7 @@ const Card = ({
       onMouseLeave={() => setShowButton(false)}>
       <h1 className='card__item__heading'>{nom}</h1>
       <div className='card__item__image-container'>
-        <img src={image_url} alt={nom} />
+        <img src={image} alt={nom} />
       </div>
       <div className='card__item__details'>
         <p>
@@ -79,14 +79,14 @@ const Card = ({
                 dispath(
                   addProduct({
                     nom,
-                    image_url,
+                    image,
                     prix,
                     id,
                     quantite: 1,
                     categories,
                   })
                 );
-                dispath(addAlert({ nom, image_url, id: uuidv4() }));
+                dispath(addAlert({ nom, image, id: uuidv4() }));
               }}>
               <OverlayTrigger
                 placement='top'
@@ -110,7 +110,7 @@ const Card = ({
       <Modal showModal={show} setShowModal={setShow} handleClose={handleClose}>
         <Modal.Header>
           <h1>{nom}</h1>
-          <img src={image_url} alt={nom} />
+          <img src={image} alt={nom} />
           <p>{splitPrix(prix)}</p>
         </Modal.Header>
         <Modal.Body>
@@ -149,9 +149,9 @@ const Card = ({
             disabled={quantite === 0}
             onClick={() => {
               dispath(
-                addProduct({ nom, image_url, prix, id, quantite, categories })
+                addProduct({ nom, image, prix, id, quantite, categories })
               );
-              dispath(addAlert({ nom, image_url, id: uuidv4() }));
+              dispath(addAlert({ nom, image, id: uuidv4() }));
               setQuantite(1);
             }}
             variant='contained'
